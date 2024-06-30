cbf25507c11a2e74eea0a5bb9e10559d2dbbefa0
Joseph "Jessy" Collins <156174614+collinsjosephj@users.noreply.github.com>
2024-06-30 14:59:00 -0700

    converted auth.js to graphQL, and converted the server.js to accept apollo server as middleware

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,35 +1,28 @@
+// Updated auth.js for GraphQL 
 const jwt = require('jsonwebtoken');
-
-// set token secret and expiration date
 const secret = 'mysecretsshhhhh';
 const expiration = '2h';
 
 module.exports = {
-  // function for our authenticated routes
-  authMiddleware: function (req, res, next) {
-    // allows token to be sent via  req.query or headers
-    let token = req.query.token || req.headers.authorization;
+  authMiddleware: function ({ req }) {
+    let token = req.token || req.query.token || req.headers.authorization;
 
-    // ["Bearer", "<tokenvalue>"]
     if (req.headers.authorization) {
       token = token.split(' ').pop().trim();
     }
 
     if (!token) {
-      return res.status(400).json({ message: 'You have no token!' });
+      return req;
     }
 
-    // verify token and get user data out of it
     try {
       const { data } = jwt.verify(token, secret, { maxAge: expiration });
       req.user = data;
     } catch {
       console.log('Invalid token');
-      return res.status(400).json({ message: 'invalid token!' });
+      return req;
     }
 
-    // send to next endpoint
-    next();
   },
   signToken: function ({ username, email, _id }) {
     const payload = { username, email, _id };
